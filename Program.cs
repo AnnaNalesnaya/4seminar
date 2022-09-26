3f50a02c3ad249e59d5d154ba6889cc4e373c959
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-09-26 19:47:32 +0300

    задача 27

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает 
+//на вход число и выдаёт сумму цифр в числе. 
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string readValue = Console.ReadLine();
+    int result = int.Parse(readValue);
+    return result;
+}
+int SumOfNambers(int numbr)
+{
+    int result = 0;
+    while (numbr > 0)
+    {
+     result += numbr % 10;
+     numbr = numbr / 10;       
+    }
+     return result;
+}
+int num = Prompt("введите число ");
+     if (num >= 0)
+{
+    Console.WriteLine($" {num} -> {SumOfNambers(num)}");
+}
+else Console.WriteLine("не корректный ввод");
